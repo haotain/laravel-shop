2d93fc20bbf4488b4d5243220441cc569c029992
haotian <872789604@qq.com>
2021-04-25 00:13:17 +0800

    fix

--- app/Admin/Controllers/UsersController.php
+++ app/Admin/Controllers/UsersController.php
@@ -4,6 +4,7 @@ namespace App\Admin\Controllers;
 
 use App\Models\User;
 use Encore\Admin\Controllers\AdminController;
+use Encore\Admin\Facades\Admin;
 use Encore\Admin\Form;
 use Encore\Admin\Grid;
 use Encore\Admin\Show;
@@ -24,6 +25,7 @@ class UsersController extends AdminController
      */
     protected function grid()
     {
+        // dd(debug_backtrace());
         $grid = new Grid(new User());
 
         // $grid->column('id', __('Id'));
