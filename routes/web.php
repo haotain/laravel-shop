16b11c4ff39ebbf12281ab01e22bcdaa9e5f34b3
haotian <872789604@qq.com>
2021-04-26 22:29:12 +0800

    用户界面 - 收藏商品列表

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 // Route::get('/', 'PagesController@root')->name('root')->middleware('verified');
-Route::redirect('/', '/products')->name('root');
-Route::get('products', 'ProductsController@index')->name('products.index');
-Route::get('products/{product}', 'ProductsController@show')->name('products.show');
+
+
+// 增加verify 参数 邮箱验证
+Auth::routes(['verify' => true]);
+
 
 // auth 中间件代表需要登录, verified 中间件代表需要经过邮箱验证
 Route::group(['middleware' => ['auth', 'verified']], function() {
@@ -37,9 +39,14 @@ Route::group(['middleware' => ['auth', 'verified']], function() {
     Route::post('products/{product}/favorite', 'ProductsController@favor')->name('products.favor');
     // 删除收藏
     Route::delete('products/{product}/favorite', 'ProductsController@disfavor')->name('products.disfavor');
+    // 收藏列表
+    Route::get('products/favorites', 'ProductsController@favorites')->name('products.favorites');
+
 
 });
 
-// 增加verify 参数 邮箱验证
-Auth::routes(['verify' => true]);
+Route::redirect('/', '/products')->name('root');
+Route::get('products', 'ProductsController@index')->name('products.index');
+Route::get('products/{product}', 'ProductsController@show')->name('products.show');
+
 
