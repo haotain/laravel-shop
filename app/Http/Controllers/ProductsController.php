b2a487d381b85f6969fca544b1890cea8d5f0d2d
haotian <872789604@qq.com>
2021-05-18 17:15:59 +0800

    顶部导航加入商品类目树

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -6,6 +6,7 @@ use App\Exceptions\InvalidRequestException;
 use App\Models\Category;
 use App\Models\OrderItem;
 use App\Models\Product;
+use App\Services\CategoryService;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -10,7 +10,18 @@
 
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <!-- left Side Of Navbar -->
-      <ul class="nvabar-nav mr-auto">
+      <ul class="navbar-nav mr-auto">
+      <!-- 顶部类目菜单开始 -->
+      <!-- 判断模板是否有 $categoryTree 变量 -->
+      @if(isset($categoryTree))
+        <li class="nav-item dropdown">
+          <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" id="categoryTree">所有类目 <b class="caret"></b></a>
+          <ul class="dropdown-menu" aria-labelledby="categoryTree">
+            <!-- 遍历 $categoryTree 集合，将集合中的每一项以 $category 变量注入 layouts._category_item 模板中并渲染 -->
+            @each('layouts._category_item', $categoryTree, 'category')
+          </ul>
+        </li>
+      @endif
       </ul>
 
       <!-- Right Side Of navbar -->
