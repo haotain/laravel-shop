0cd5e9c4b57f878175d19a2f08a7f1e40a735e32
haotian <872789604@qq.com>
2021-06-08 23:01:50 +0800

    商品属性模型

---
+++ app/Models/ProductProperty.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ProductProperty extends Model
+{
+    use HasFactory;
+
+    // 没有 created_at 和 updated_at 字段
+    public $timestamps = false;
+
+    protected $fillable = ['name', 'value'];
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+
+}
